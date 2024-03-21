4dbef53ed287fc965e70c69c1254cbfab34bcd78
classuser199 <oleksandr.i.vyrsta@ukd.edu.ua>
2024-03-21 13:18:35 +0200

    compleated all dictionary

--- JoJoLion.py
+++ JoJoLion.py
@@ -5,6 +5,8 @@ def info():
         if a == "Dio" or a == "dio":
             dios = DIO()
             print(f"Name: {dios.Name}\nAge: {dios.Age}\nStand: {dios.Stand}\nParts: {dios.Parts}")
+            print(dioskils.keys)
+            print(dioskils.values)
         elif a == "Kars" or a == "kars":
             karsus = KARS()
             print(f"Name: {karsus.Name}\nAge: {karsus.Age}\nStand: {karsus.Stand}\nParts: {karsus.Parts}")
@@ -17,9 +19,10 @@ def info():
         elif a == "Pucci" or a == "pucci":
             puccis = PUCCI()
             print(f"Name: {puccis.Name}\nAge: {puccis.Age}\nStand: {puccis.Stand}\nParts: {puccis.Parts}")
-        b = str(input("Ви хочите продовжити?"))
-        if b == "No" or b == "no" or b == "Ні" or b =="ні":
+        b = str(input("Ви впевнені в виборі?"))
+        if b == "Yes" or b == "yes" or b == "Так" or b =="так":
             menu_is_running = (False)
+
 
 class DIO:
     Name = "Dio Burandō"
@@ -27,11 +30,11 @@ class DIO:
     Stand = "The World"
     Parts = "Part 1, Part 3, Par 6"
     dioskils = {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
-        5: "",
+        1: "Block",
+        2: "Donut",
+        3: "Knives",
+        4: "Time Stop",
+        5: "Road Roller Da",
     }
 
 class KARS:
@@ -40,11 +43,11 @@ class KARS:
     Stand = "None"
     Parts = "Part 2"
     karsskils = {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
-        5: "",
+        1: "Intake",
+        2: "Punch",
+        3: "Strong rush-down",
+        4: "Light Blades",
+        5: "Ultimate Thing",
     }
 
 class YOSHIKAGE:
@@ -53,11 +56,11 @@ class YOSHIKAGE:
     Stand = "Killer Queen"
     Parts = "Part 4"
     yoshikageskils = {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
-        5: "",
+        1: "Block",
+        2: "Coin",
+        3: "Stray cat",
+        4: "Sheer Heart Attack",
+        5: "Bite The Dust",
     }
 
 class DIAVOLO:
@@ -66,11 +69,11 @@ class DIAVOLO:
     Stand = "King Crimson"
     Parts = "Part 5"
     diavoloskils = {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
-        5: "",
+        1: "Punch",
+        2: "Donut",
+        3: "Blood in the eyes",
+        4: "Time Erasure",
+        5: "Epitaph",
     }
 class PUCCI:
     Name = "Enrico Pucci"
@@ -78,11 +81,11 @@ class PUCCI:
     Stand = "Whitesnake, C-Moon, Made in Heaven"
     Parts = "Part 6"
     pucciskils = {
-        1: "",
-        2: "",
-        3: "",
-        4: "",
-        5: "",
+        1: "Illusions",
+        2: "Whitesnake Punch",
+        3: "C-Moon Punch",
+        4: "Take The Stand Disk",
+        5: "Reboot",
     }
 
 info()
